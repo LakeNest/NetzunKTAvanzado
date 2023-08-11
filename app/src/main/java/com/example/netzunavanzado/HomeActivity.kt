44a7d9a5c5a287d8cd22249edd92df6f3e8d54a9
nesnieri <nraulnieri@gmail.com>
2023-08-11 18:37:13 -0500

    Pase otro fragment en ViewHolder del Adapter

--- app/src/main/java/com/example/netzunavanzado/HomeActivity.kt
+++ app/src/main/java/com/example/netzunavanzado/HomeActivity.kt
@@ -38,7 +38,7 @@ class HomeActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_fase, R.id.nav_detail_fase
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
---
+++ app/src/main/java/com/example/netzunavanzado/ui/fase/FaseFragment.kt
@@ -0,0 +1,76 @@
+package com.example.netzunavanzado.ui.fase
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.netzunavanzado.R
+import com.example.netzunavanzado.databinding.FragmentFaseBinding
+
+class FaseFragment : Fragment() {
+    private var _binding: FragmentFaseBinding? = null
+    private val binding get() = _binding!!
+    private var mUsuList: MutableList<Usuario> = mutableListOf()
+    private lateinit var adapter: UsuarioAdapter
+    var llmanager = LinearLayoutManager(activity)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFaseBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        mUsuList.add(Usuario("juan","quispe"))
+        mUsuList.add(Usuario("raul","nieri"))
+
+
+        initRecycler()
+
+
+
+
+
+
+        return root
+    }
+    fun initRecycler() {
+        adapter = UsuarioAdapter(mUsuList,
+            onClick = { usu -> itemSelected(usu) })
+        //val manager = LinearLayoutManager(this)
+        //val decoration = DividerItemDecoration(this,manager.orientation )
+        with(binding) {
+            rvUsuarios.layoutManager = llmanager
+            rvUsuarios.adapter = adapter
+            //rvSuperHero.addItemDecoration(decoration)
+        }
+    }
+    private fun createUsuario() {
+        var usuario1 = Usuario(
+            "devsconocido",
+            "??????",
+        )
+        mUsuList.add(1,usuario1)
+        adapter.notifyItemInserted(1)
+        llmanager.scrollToPositionWithOffset(1,10)
+    }
+
+    private fun itemSelected(usu: Usuario) {
+        //Toast.makeText(activity, usu.nombre, LENGTH_LONG).show()
+        val bundle = Bundle()
+        bundle.putString("dato2", "Foo");
+        val navController = findNavController()
+        navController.navigate(
+            R.id.nav_detail_fase, bundle, NavOptions.Builder()
+            .setPopUpTo(R.id.nav_home, true)
+            .build())
+    }
+
+}
