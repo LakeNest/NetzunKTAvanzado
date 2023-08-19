9c9a356327cdadac90a84076cf7e2127a88af76a
nesnieri <nraulnieri@gmail.com>
2023-08-19 00:22:45 -0500

    Admob BANNER y Payments Stripe

--- app/src/main/java/com/example/netzunavanzado/CheckoutActivity.kt
+++ app/src/main/java/com/example/netzunavanzado/CheckoutActivity.kt
@@ -159,7 +159,7 @@ class CheckoutActivity : AppCompatActivity() {
                 shippingDetails = result.address
             }
             is AddressLauncherResult.Canceled -> {
-               
+
             }
         }
     }
